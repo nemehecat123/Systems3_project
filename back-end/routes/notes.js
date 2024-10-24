6fcaf734809637a4246237b869002145063ff995
Matevz <matevz4ever@gmail>
2024-10-24 20:12:38 +0200

    adding a function that gets all notes

--- back-end/routes/notes.js
+++ back-end/routes/notes.js
@@ -37,4 +37,32 @@ notes.get('/', async (req, res, next) => {
     }
 });
 
+
+notes.get('/getNotes', async (req, res, next) => {
+    const authHeader = req.headers['authorization']; // Get the Authorization header
+  const id_classes = authHeader.split(' ')[1]; // Extract user ID from token
+  console.log("id_classes :", id_classes);
+
+  try {
+    const queryResult = await DB.getAllNotesForUser(id_classes); // id_classes stevilko rabis da vidis "Listek"
+    if (queryResult.length === 0) {
+      return res.status(404).json({ success: false, msg: "Note not found" });
+    }
+
+    const fileData = queryResult[0].file_blob;  // Assuming the file blob is stored in a `file_blob` column
+    const fileName = queryResult[0].file_name || 'downloaded_file';  // Assuming you store the filename too
+
+    // Setting headers to return the file correctly
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+    res.setHeader('Content-Type', 'application/octet-stream');
+
+    // Return the note data
+    res.json(queryResult[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, msg: "Server error" });
+    next();
+      }
+  });
+
 module.exports = notes;
